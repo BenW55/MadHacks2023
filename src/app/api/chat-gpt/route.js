8a8f6086a607745b89aa985c232b4970774478f0
BenW55 <bcar47905@gmail.com>
2023-11-11 15:25:24 -0600

    gpt response

--- src/app/api/chat-gpt/route.js
+++ src/app/api/chat-gpt/route.js
@@ -4,12 +4,30 @@ import OpenAI from "openai";
 export async function POST(request)
 {
 
-    console.log(process.env.OPENAI_API_KEY);
 
     const openai = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY
     });
-    return NextResponse.json({name: "WORKS",
 
-})
+    const params = await request.json();
+
+    const response = await openai.chat.completions.create({
+        model: "gpt-4",
+        messages: [
+            {
+                role: "system",
+                content: "You are reviewing a resume"
+            },
+            {
+                role:"user",
+                content: params.prompt
+            }
+        ],
+        temperature: 0,
+        max_tokens: 1024,
+        top_p:1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    }) 
+    return NextResponse.json(response);
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,40 @@
 "use client";
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react';
+import PromptForm from '@/components/PromptForm';
 
 export default function Home() {
+
+  const [choices, setChoices] = useState([]);
+
   return (
     <main className={styles.main}>
       <p>Hi!</p>
+      <PromptForm
+              onSubmit={ async(prompt) => {
+                const response =  await fetch("/api/chat-gpt", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    prompt: prompt,
+        
+                  }),
+                });
+                const result = await response.json();
+                setChoices(result.choices)
+        
+              }}/>
 
+      {choices.map(choice => {
+        return (
+          <p kep = {choice.index}>{choice.message.content}</p>
+        )
+      })
 
-      <button onClick={async () =>{
-        const response =  await fetch("/api/chat-gpt", {
-          method: "POST",
-          headers: {
-            "Content-Type": "applicatoin/json",
-          },
-          body: JSON.stringify({
-            someData:true,
-
-          }),
-        });
-        console.log("RESPONSE", response);
-
-
-      }}>
-        Hit API
-      </button>
+      }
     </main>
   )
 }
